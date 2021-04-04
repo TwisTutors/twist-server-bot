3103ba78c13deffdc6559862b57524797a6af1f4
Sam_Ash11414 <72979934+SamAsh11414@users.noreply.github.com>
2021-04-04 15:29:27 -0400

    Fixed issues in tictactoe

--- TwistTutors.py
+++ TwistTutors.py
@@ -46,7 +46,7 @@ async def assist(ctx):
     )
     assist_embed.add_field(name="Help", value="`;assist`", inline=True)
     assist_embed.add_field(name="View our website", value="`;web`", inline=True)
-    assist_embed.add_field(name="Ask for help", value="`;help`")
+    assist_embed.add_field(name="Ask for help", value="`;help (subject)`")
     await ctx.send(embed=assist_embed)
 
 
@@ -176,62 +176,67 @@ async def game(ctx, subject):
             if Your_Move.content == "A1" and A1 == ":white_large_square:":
                 A1 = ":x:"
                 AIPlay.remove("A1")
-            if Your_Move.content == "A2" and A2 == ":white_large_square:":
+            elif Your_Move.content == "A2" and A2 == ":white_large_square:":
                 A2 = ":x:"
                 AIPlay.remove("A2")
-            if Your_Move.content == "A3" and A3 == ":white_large_square:":
+            elif Your_Move.content == "A3" and A3 == ":white_large_square:":
                 A3 = ":x:"
                 AIPlay.remove("A3")
-            if Your_Move.content == "B1" and B1 == ":white_large_square:":
+            elif Your_Move.content == "B1" and B1 == ":white_large_square:":
                 B1 = ":x:"
                 AIPlay.remove("B1")
-            if Your_Move.content == "B2" and B2 == ":white_large_square:":
+            elif Your_Move.content == "B2" and B2 == ":white_large_square:":
                 B2 = ":x:"
                 AIPlay.remove("B2")
-            if Your_Move.content == "B3" and B3 == ":white_large_square:":
+            elif Your_Move.content == "B3" and B3 == ":white_large_square:":
                 B3 = ":x:"
                 AIPlay.remove("B3")
-            if Your_Move.content == "C1" and C1 == ":white_large_square:":
+            elif Your_Move.content == "C1" and C1 == ":white_large_square:":
                 C1 = ":x:"
                 AIPlay.remove("C1")
-            if Your_Move.content == "C2" and C2 == ":white_large_square:":
+            elif Your_Move.content == "C2" and C2 == ":white_large_square:":
                 C2 = ":x:"
                 AIPlay.remove("C2")
-            if Your_Move.content == "C3" and C3 == ":white_large_square:":
+            elif Your_Move.content == "C3" and C3 == ":white_large_square:":
                 C3 = ":x:"
                 AIPlay.remove("C3")
-            if Your_Move.content == "Quit":
+            elif Your_Move.content == "Quit":
                 break
+            else: 
+                continue
+
             Move = Move + 1
             print(AIPlay)
             AIPlayedMove = random.choice(AIPlay)
             if AIPlayedMove == "A1" and A1 == ":white_large_square:":
                 A1 = ":o:"
                 AIPlay.remove("A1")
-            if AIPlayedMove == "A2" and A2 == ":white_large_square:":
+            elif AIPlayedMove == "A2" and A2 == ":white_large_square:":
                 A2 = ":o:"
                 AIPlay.remove("A2")
-            if AIPlayedMove == "A3" and A3 == ":white_large_square:":
+            elif AIPlayedMove == "A3" and A3 == ":white_large_square:":
                 A3 = ":o:"
                 AIPlay.remove("A3")
-            if AIPlayedMove == "B1" and B1 == ":white_large_square:":
+            elif AIPlayedMove == "B1" and B1 == ":white_large_square:":
                 B1 = ":o:"
                 AIPlay.remove("B1")
-            if AIPlayedMove == "B2" and B2 == ":white_large_square:":
+            elif AIPlayedMove == "B2" and B2 == ":white_large_square:":
                 B2 = ":o:"
                 AIPlay.remove("B2")
-            if AIPlayedMove == "B3" and B3 == ":white_large_square:":
+            elif AIPlayedMove == "B3" and B3 == ":white_large_square:":
                 B3 = ":o:"
                 AIPlay.remove("B3")
-            if AIPlayedMove == "C1" and C1 == ":white_large_square:":
+            elif AIPlayedMove == "C1" and C1 == ":white_large_square:":
                 C1 = ":o:"
                 AIPlay.remove("C1")
-            if AIPlayedMove == "C2" and C2 == ":white_large_square:":
+            elif AIPlayedMove == "C2" and C2 == ":white_large_square:":
                 C2 = ":o:"
                 AIPlay.remove("C2")
-            if AIPlayedMove == "C3" and C3 == ":white_large_square:":
+            elif AIPlayedMove == "C3" and C3 == ":white_large_square:":
                 C3 = ":o:"
                 AIPlay.remove("C3")
+
+
 
 
 
